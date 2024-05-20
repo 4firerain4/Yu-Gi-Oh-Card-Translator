2b10b22ff83b934d7e4010416a1e32b17426cd7b
4firerain4 <4firerain4@gmail.com>
2024-05-20 06:15:36 +0300

    Cleared unusable code + test new branch

--- Program.cs
+++ Program.cs
@@ -7,27 +7,26 @@ namespace Yu_Gi_Oh_Card_Translator
 
         static async Task Main(string[] args)
         {
-            string CacheDir = AppDomain.CurrentDomain.BaseDirectory;
-            Console.ReadKey();
             var a = new StreamReader(@"D:\WORK\Yu-Gi-Oh-Card-Translator\my deck 1.ydk");
             string[] unsortedCodes = a.ReadToEnd().Split("\r\n");
             var codes = unsortedCodes.Where(p => int.TryParse(p, out _)).ToArray();
 
             List<Card> cards = new();
 
-            if (CsvContext.CheckCache()) cards = await CsvContext.ReadDataAsync();
-            else
+            if (!CsvContext.CheckCache())
             {
                 cards = await ParseData.ParseTextAsync(codes);
                 cards = await Translator.TranslateTextAsync(cards);
-                //await DownloadImages.DownloadImagesAsync(codes);
                 CsvContext.WriteData(cards);
-                cards = await CsvContext.ReadDataAsync();
+                await DownloadImages.DownloadImagesAsync(codes);
+                await ImageManipilator.ClearText(cards);
             }
 
+            cards = await CsvContext.ReadDataAsync();
             cards = cards.GroupBy(x => x.Code).Select(group => group.First()).ToList();
-            //await ImageManipilator.ClearText(cards);
-            await ImageManipilator.PrepareText(cards);
+
+            await ImageManipilator.DrawText(cards);
+            Console.WriteLine("DONE");
             Console.ReadKey();
         }
 
